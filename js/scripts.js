a230fa51d5dbd4437aef7aa630e62dbc931a7b45
unknown <aguilarjacob23@gmail.com>
2018-10-24 20:44:58 -0700

    Nav and Carousel complete

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,24 @@ const nav = document.querySelector('#navigation');
 const navTop = nav.offsetTop;
 
 // need to know when to activate the sticky nav bar
-function stickyNavigationpx() {
-  console.log(`navTop = ${navTop}`)
-  console.log(`scrollY = ${window.scrollY}`);  
-}
+// function stickyNavigationpx() {
+//   console.log(`navTop = ${navTop}`)
+//   console.log(`scrollY = ${window.scrollY}`);  
+// }
 
-window.addEventListener('scroll', stickyNavigationpx);
+// window.addEventListener('scroll', stickyNavigationpx);
+
 
 // trigger sticky nav by adding css class to stylesheet when scrollY is greater than navTop
 function stickyNavigation(){
     if(window.scrollY > navTop){
         // document.body.style.paddingTop = nav.offsetHeight + 'px';
         document.getElementById("navigation").classList.add('fixed-nav');
+        document.getElementById("navigation").classList.remove('fixed-nav-out');
+
     } else{
-        document.body.style.paddingTop = 0;
-        document.getElementById("navigation").classList.remove('fixed-nav');
+        document.getElementById("navigation").classList.remove('fixed-nav')
+        document.getElementById("navigation").classList.add('fixed-nav-out');
     }
 }
 
